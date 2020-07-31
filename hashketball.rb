a01505f42451a9659f5d202065317931a703b6c7
jade decordova <lacroixjaded@gmail.com>
2020-07-31 18:50:02 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,12 +1,12 @@
 # Write your code here!
 def game_hash
   hash = {
-  :home => {:team_name => {""},
+  :home => {:team_name => {},
             :colors => {},
             :players => {} 
     
   }, 
-  :away => {:team_name => {''},
+  :away => {:team_name => {},
             :colors =>{},
             :players => {}
     
